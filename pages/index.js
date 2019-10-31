c7859164f46942931c01f7f8030d3f4c240d9daa
henriqueFutema <henrique.futema01@gmail.com>
2019-10-31 20:03:00 -0300

    Head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+
 import Link from "next/link";
+import Head from "next/head";
 
 const Home = () => (
   <div>
+    <Head>
+      <title>Home</title>
+    </Head>
     <h1>Hello</h1>
     <Link href="/user">
       <a>Users</a>
